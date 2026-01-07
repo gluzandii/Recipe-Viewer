8ba8889452397e2463b6d702ab8620ea3a73b141
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-07 21:44:06 +0530

    Added pic.
    The pic coems up into the screen and animates smoothly.
    
    Next to do add text around it.

--- frontend-rv/src/pages/home/Home.tsx
+++ frontend-rv/src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import styles from "./Home.module.scss"
+import pic from "../../assets/recipe-home.jpg"
 
 export default function Home() {
     return (
@@ -9,6 +10,14 @@ export default function Home() {
                 <p>Ingredients</p>
                 <button className={styles.loginButton}>Login</button>
             </nav>
+            <div className={styles.frontImageWrapper}>
+                <img
+                    src={pic}
+                    alt="Yummy food pic failed to load :("
+                    className={styles.frontImage}
+                />
+            </div>
+
         </main>
     );
 }
