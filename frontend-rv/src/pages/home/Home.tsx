01819c26e0b2190e1c500b4afcdd4844efc7b86d
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-07 23:49:51 +0530

    Adds homepage layout and basic animations
    Implements the initial layout for the homepage, including a visual representation and descriptive text.
    
    Introduces animations to enhance the user experience when the page loads.
    
    Moves navigation bar styles to its own module.

--- frontend-rv/src/pages/home/Home.tsx
+++ frontend-rv/src/pages/home/Home.tsx
@@ -1,21 +1,30 @@
 import styles from "./Home.module.scss"
+import navbarStyles from "./Navbar.module.scss"
 import pic from "../../assets/recipe-home.jpg"
 
 export default function Home() {
     return (
         <main>
-            <nav className={styles.navbar}>
+            <nav className={navbarStyles.navbar}>
                 <p>Home</p>
                 <p>Recipes</p>
                 <p>Ingredients</p>
-                <button className={styles.loginButton}>Login</button>
+                <button className={navbarStyles.loginButton}>Login</button>
             </nav>
-            <div className={styles.frontImageWrapper}>
+            <div className={styles.homePage}>
+                <div className={styles.firstDiv}>
+                    <p>Recipe</p>
+                    <p>🍳🥗🍜</p>
+                </div>
                 <img
                     src={pic}
                     alt="Yummy food pic failed to load :("
                     className={styles.frontImage}
                 />
+                <div className={styles.secondDiv}>
+                    <p>📖✅📝</p>
+                    <p>Viewer</p>
+                </div>
             </div>
         </main>
     );
