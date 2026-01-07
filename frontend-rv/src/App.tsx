a8b8b0b680e7802fb3af1f2192d4c32df9132a23
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-08 01:13:38 +0530

    Start work on ingredients route.

--- frontend-rv/src/App.tsx
+++ frontend-rv/src/App.tsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/home/Home.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Recipes from "./pages/recipes/Recipes.tsx";
+import Ingredients from "./pages/ingredients/Ingredients.tsx";
 
 export default function App() {
     return (
@@ -12,7 +13,7 @@ export default function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/recipes" element={<Recipes/>}/>
                 <Route path="/recipe/:recipeID" element={<div>recipe</div>}/>
-                <Route path="/ingredients/:ingredID" element={<div>ingredients</div>}/>
+                <Route path="/ingredients/:ingredID" element={<Ingredients/>}/>
                 <Route path="*" element={<div>not found</div>}/>
             </Routes>
         </main>
