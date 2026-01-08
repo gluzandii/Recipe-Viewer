deca50a60709cd866491543c1848fc0e1acd9f75
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-08 21:32:36 +0530

    Backend serves frontend now

--- backend-rv/src/app.ts
+++ backend-rv/src/app.ts
@@ -4,6 +4,8 @@ import {authRouter} from "./routes/auth.js";
 import {recipesRouter} from "./routes/recipes.js";
 import {ingredientsRouter} from "./routes/ingredients.js";
 import cors from "cors";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
 
 export const app: express.Express = express();
 
@@ -13,6 +15,15 @@ app.use(cors({
     origin: `http://localhost:5173`,
     credentials: true,
 }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get(/.*/, (req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+})
 
 app.use("/api/auth", authRouter);
 app.use("/api/recipes", recipesRouter);
