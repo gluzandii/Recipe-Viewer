31bbe1c9cbc5ac421c15f15906e511b51dd5b914
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-08 21:08:55 +0530

    Remove all RV_ ports and fix 3000 for backend 5432 for postrgres and 5173 for frontend

--- backend-rv/src/app.ts
+++ backend-rv/src/app.ts
@@ -4,19 +4,13 @@ import {authRouter} from "./routes/auth.js";
 import {recipesRouter} from "./routes/recipes.js";
 import {ingredientsRouter} from "./routes/ingredients.js";
 import cors from "cors";
-import "dotenv/config"
 
 export const app: express.Express = express();
-if (!process.env.RV_FRONT) {
-    console.error("RV_FRONT environment variable is not set.");
-    process.exit(1)
-}
-const RV_FRONT = process.env.RV_FRONT;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: `http://localhost:${RV_FRONT}`,
+    origin: `http://localhost:5173`,
     credentials: true,
 }));
 
--- backend-rv/src/index.ts
+++ backend-rv/src/index.ts
@@ -1,12 +1,6 @@
 import {app} from "./app.js";
 
-if (!process.env.RV_BACK) {
-    console.error("RV_BACK not set in environment variables, exiting.");
-    process.exit(1);
-}
 
-const PORT = Number(process.env.RV_BACK);
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log(`Server running on http://localhost:3000`);
 });
